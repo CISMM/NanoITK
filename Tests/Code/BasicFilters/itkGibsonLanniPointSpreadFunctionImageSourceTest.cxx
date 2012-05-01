7da545f27ed0d247d89de69feb733c12aa78766f
Cory Quammen <cquammen@cs.unc.edu>
2012-05-01 17:30:25 -0400

    Fleshed out the tests to save images generated from the default settings.

--- Tests/Code/BasicFilters/itkGibsonLanniPointSpreadFunctionImageSourceTest.cxx
+++ Tests/Code/BasicFilters/itkGibsonLanniPointSpreadFunctionImageSourceTest.cxx
@@ -1,16 +1,38 @@
 #include "itkImage.h"
+#include "itkImageFileWriter.h"
 #include "itkGibsonLanniPointSpreadFunctionImageSource.h"
 
 int main(int argc, char* argv[])
 {
+  if ( argc < 2 )
+    {
+    std::cout << "Usage: " << argv[0] << " <output file name>" << std::endl;
+    return EXIT_FAILURE;
+    }
 
-  typedef float PixelType;
+  typedef double PixelType;
   const unsigned int Dimension = 3;
   typedef itk::Image< PixelType, Dimension> ImageType;
   typedef itk::GibsonLanniPointSpreadFunctionImageSource< ImageType > SourceType;
   typedef SourceType::Pointer SourcePointer;
 
   SourcePointer source = SourceType::New();
+  source->Print( std::cout );
+
+  // Save image with default parameters
+  typedef itk::ImageFileWriter< ImageType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName( argv[1] );
+  writer->SetInput( source->GetOutput() );
+
+  try
+    {
+    writer->Update();
+    }
+  catch ( itk::ExceptionObject & except )
+    {
+    std::cerr << "Caught an exception: " << except << std::endl;
+    }
 
   return EXIT_SUCCESS;
 }
--- Tests/Code/BasicFilters/itkHaeberlePointSpreadFunctionImageSourceTest.cxx
+++ Tests/Code/BasicFilters/itkHaeberlePointSpreadFunctionImageSourceTest.cxx
@@ -1,8 +1,14 @@
 #include "itkImage.h"
+#include "itkImageFileWriter.h"
 #include "itkHaeberlePointSpreadFunctionImageSource.h"
 
 int main(int argc, char* argv[])
 {
+  if ( argc < 2 )
+    {
+    std::cout << "Usage: " << argv[0] << " <output file name>" << std::endl;
+    return EXIT_FAILURE;
+    }
 
   typedef float PixelType;
   const unsigned int Dimension = 3;
@@ -11,6 +17,22 @@ int main(int argc, char* argv[])
   typedef SourceType::Pointer SourcePointer;
 
   SourcePointer source = SourceType::New();
+  source->Print( std::cout );
+
+  // Save image with default parameters
+  typedef itk::ImageFileWriter< ImageType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName( argv[1] );
+  writer->SetInput( source->GetOutput() );
+
+  try
+    {
+    writer->Update();
+    }
+  catch ( itk::ExceptionObject & except )
+    {
+    std::cerr << "Caught an exception: " << except << std::endl;
+    }
 
   return EXIT_SUCCESS;
 }
